2148f8cf902756ccb068491d2c47dc32771311ef
ken@ubuntu-vm <vuken30227@gmail.com>
2019-08-07 02:59:13 -0700

    Added comments about lifecycle hooks in index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+//LifeCycle hooks
+//There are 3 states when you launch a react app
+//Mount - gets called at the start when you load a component
+//Update - gets called when you update the state
+//Unmount - When a component removes from the DOM (e.g when you delete a counter)
+
+//In each of these states, they have functions where you can do stuff with
+//Mount - (in this order) constructor, render, componentDidMount
+//Update - render, componentDidUpdate
+//Unmount - componentWillUnmount
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
